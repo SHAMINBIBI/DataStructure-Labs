ec6c7c06dc2420e4750ed568bb222784d11aa5b3
ShaminBibi-engr <khawarshamin@gmail.com>
2026-02-28 23:09:48 +0500

    LinkedListReverse ByKGroup

--- LAB6/LinkedListkReversal.cpp
+++ LAB6/LinkedListkReversal.cpp
@@ -1,16 +1,14 @@
 #include <iostream>
 using namespace std;
 struct node {
-    int val;
+    int d;
     node *next;
-    node() : val(0), next(nullptr) {}
-    node(int x) : val(x), next(nullptr) {}
-    node(int x, node *next) : val(x), next(next) {}
+    node() : d(0), next(nullptr) {}
+    node(int x) :d(x), next(nullptr) {}
+    node(int x, node *next) :d(x), next(next) {}
 };
-class linkedList
-{
-	node* head;
-
+class linkedList   //linked list class that was made in LAB 2
+{	node* head;
 public:
 	linkedList() : head(NULL)	{}
 	linkedList(const linkedList &obj)
@@ -18,9 +16,50 @@ public:
         node* temp = obj.head;
 		while(temp!=NULL)
 		{
-		  insertAtEnd(temp->val);
+		  insertAtEnd(temp->d);
 		  temp = temp->next;
 		}
+	}
+	void display()
+	{
+		node* temp = head;
+		while (temp!=NULL)
+		{
+			cout << temp->d << " ";
+			temp = temp->next;
+		}
+		cout << endl;
+	}	
+	void insertAtEnd(int d)
+	{	node* n = new node;
+		n->d = d;		
+		if (head == NULL)
+		{
+			head = n;
+			return;
+		}		
+		node* temp = head;
+		while (temp->next != NULL)
+		{
+			temp = temp->next;
+		}
+		temp->next = n;
+	}
+	int getLength()
+	{	int count = 0;
+		node* temp = head;	
+		while (temp != NULL)
+		{
+			count++;
+			temp = temp->next;
+		}	
+		return count;
+	}
+	node* getHead() { return head; }
+    void setHead(node* newHead) { head = newHead; }
+	bool isEmpty()
+	{
+		return (head == NULL);
 	}
 	void deleteFromStart()
 	{
@@ -35,143 +74,6 @@ public:
 			cout << "LIST IS EMPTY!" << endl;
 		}
 	}
-    void deleteFromAnyPos(int pos)
-	{
-		if(pos<1 || pos> getLength())
-		{  cout<< "INVALID POSITION ENTERED-ONLY POSITIONS BETWEEN 1 AND " << getLength()<< " ARE ALLOWED\n";
-		return;
-	    }
-		if(pos==1)
-		{
-			deleteFromStart();
-		}
-		else if (pos==getLength())
-		{ deleteFromEnd();}
-		else
-		{
-			node* temp= head;
-			for(int i=1;i<pos-1;i++)
-			{
-				temp =temp->next;
-			}
-			node* toBeDeleted = temp->next;
-			temp->next= toBeDeleted->next;
-			delete toBeDeleted;
-		}
-
-	}
-	void deleteFromEnd()
-	{
-		if (head == NULL)
-		{
-			cout << "LIST IS EMPTY!" << endl;
-			return;
-		}		
-		if (head->next == NULL)
-		{
-			delete head;
-			head = NULL;
-			return;
-		}		
-		node* temp = head;
-		while (temp->next->next != NULL)
-		{
-			temp = temp->next;
-		}		
-		delete temp->next;
-		temp->next = NULL;
-	}
-	void display()
-	{
-		node* temp = head;
-		while (temp!=NULL)
-		{
-			cout << temp->val << " ";
-			temp = temp->next;
-		}
-		cout << endl;
-	}	
-	void insertAtStart(int d)
-	{
-		node* n = new node;
-		(*n).val = d;
-		n->next = head;
-		head = n;
-		
-	}
-    void insertAtAnyPos(int d,int pos)
-	{   if(pos ==1)
-		{
-			insertAtStart(d);
-		}
-		if (pos < 1 || pos > getLength() + 1)
-    {
-        cout << " INVALID POSITION!!! Position should be between 1 and " 
-             << getLength() + 1 << endl;
-        return;
-    }
-		else if(pos <= getLength() + 1  && pos > 0)
-		{
-		node* n =new node;
-		n->val = d;
-		node* temp= head;
-		int count=1;
-		while(count < pos-1)
-		{
-			temp= temp-> next;
-			count ++;
-		}
-		n-> next = temp-> next;
-		temp->next = n;
-	    }   	
-	}
-	void insertAtEnd(int d)
-	{
-		node* n = new node;
-		n->val = d;		
-		if (head == NULL)
-		{
-			head = n;
-			return;
-		}		
-		node* temp = head;
-		while (temp->next != NULL)
-		{
-			temp = temp->next;
-		}
-		temp->next = n;
-	}
-	int getLength()
-	{
-		int count = 0;
-		node* temp = head;
-		
-		while (temp != NULL)
-		{
-			count++;
-			temp = temp->next;
-		}
-		
-		return count;
-	}
-	bool isEmpty()
-	{
-		return (head == NULL);
-	}
-	bool searchData(int d)
-	{
-		node* temp = head;
-		
-		while (temp != NULL)
-		{
-			if (temp->val == d)
-			{
-				return true;
-			}
-			temp = temp->next;
-		}		
-		return false;
-	}
 	~linkedList()
 	{
 		while(head!=NULL)
@@ -180,35 +82,53 @@ public:
 		}
 	}
 };
-class reversalByK {
-public:
-    node* reverseKGroup(node* head, int k) {
-        // 1. Check if there are at least k nodes left in the list
-        node* curr = head;
-        for (int i = 0; i < k; i++) {
-            if (!curr) return head; // If less than k nodes, leave them as is
-            curr = curr->next;
-        }
-
-        // 2. Reverse the first k nodes
-        node* prevNode = nullptr;
-        node* currNode = head;
-        node* nextNode = nullptr;
-
-        for (int i = 0; i < k; i++) {
-            nextNode = currNode->next; // Save next node
-            currNode->next = prevNode; // Reverse pointer
-            prevNode = currNode;       // Move prev forward
-            currNode = nextNode;       // Move curr forward
-        }
-
-        // 3. After reversal, 'head' is now the tail of this group.
-        // Connect it to the result of the next recursive call.
-        if (nextNode != nullptr) {
-            head->next = reverseKGroup(nextNode, k);
-        }
-
-        // 4. 'prevNode' is the new head of this reversed group
-        return prevNode;
-    }
+class LinkedListRev{
+    public:
+    node* reversebyKGroup(node* head,int k)
+	{
+		node* temp =head;
+		//checking if k nodes exist in linked list
+		int count = 1;
+		while(count<=k)
+		{	if(temp==NULL)
+			{ return head;}
+			temp=temp->next;
+			count++;
+		}
+		//recursive call for the rest of list i.e after k group
+		node* prevNode = reversebyKGroup(temp,k);
+		//reverse current group
+		temp = head;
+		count = 1;
+		while (count<=k)
+		{   node* next = temp->next;
+			temp->next = prevNode;
+			prevNode = temp;
+			temp = next;
+			count++;
+		}
+		return prevNode;
+	}
 };
+int main()
+{   linkedList list1;  
+    for (int i = 1; i <= 5; i++) list1.insertAtEnd(i); 
+    cout << "Original List: ";
+    list1.display();
+    LinkedListRev revTool;
+    int k = 2;   //for k=2
+    node* newHead1 = revTool.reversebyKGroup(list1.getHead(), k);  // Get the head, reverse it, and set the new head back
+    list1.setHead(newHead1);
+    cout << "After " << k << "-group reversal: ";
+    list1.display();
+    linkedList list2;
+    for (int i = 1; i <= 6; i++) list2.insertAtEnd(i);
+    cout << "Original List: ";
+    list2.display();
+    int k1 = 3;
+    node* newHead2 = revTool.reversebyKGroup(list2.getHead(), k1);  //for k=3
+    list2.setHead(newHead2);
+    cout << "After " << k1 << "-group reversal: ";
+    list2.display();
+    return 0;	
+}
